f6f522461d888f39cb9150e5bb860845dbe8e1cb
jonjowett <33575792+jonjowett@users.noreply.github.com>
2026-04-27 19:17:31 +0200

    CK2 MP: WTWSMS Compilation - Initial setup - part 5 - Initial generation

---
+++ ck2_mp_wtwsms_compilation/mod_files/ck2_mp_wtwsms_compilation/common/defines/no_artifact_losing.lua
@@ -0,0 +1,2 @@
+NDefines.NCharacter.TREASURY_CHANCE_TO_DISAPPEAR_STANDARD = 0	 	-- Chances that artifacts disappear on inheritance
+NDefines.NCharacter.TREASURY_CHANCE_TO_DISAPPEAR_NO_HEIR = 0		-- Chances that artifacts disappear on inheritance when the dying character doesn't have an heir
