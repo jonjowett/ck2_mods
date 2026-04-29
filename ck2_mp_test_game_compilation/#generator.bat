4b5ab2d931dff4c5b0c9fc71590e7ba1ee42af76
jonjowett <33575792+jonjowett@users.noreply.github.com>
2026-04-29 02:19:15 +0200

    CK2 MP: Test Game Compilation - Include ED: Plots

--- ck2_mp_test_game_compilation/#generator.bat
+++ ck2_mp_test_game_compilation/#generator.bat
@@ -33,6 +33,7 @@ xcopy /R /Y /Q /S /E /I %git_mod_folder_location%expanded_decisions_custom_empir
 xcopy /R /Y /Q /S /E /I %git_mod_folder_location%expanded_decisions_gifts%git_mod_subfolder%expanded_decisions_gifts\ %this_mod_folder_location%
 xcopy /R /Y /Q /S /E /I %git_mod_folder_location%expanded_decisions_great_projects%git_mod_subfolder%expanded_decisions_great_projects\ %this_mod_folder_location%
 xcopy /R /Y /Q /S /E /I %git_mod_folder_location%expanded_decisions_multiplayer\mod_files_compatible\expanded_decisions_multiplayer_compatible\ %this_mod_folder_location%
+xcopy /R /Y /Q /S /E /I %git_mod_folder_location%expanded_decisions_plots%git_mod_subfolder%expanded_decisions_plots\ %this_mod_folder_location%
 xcopy /R /Y /Q /S /E /I %git_mod_folder_location%expanded_decisions_prisoners%git_mod_subfolder%expanded_decisions_prisoners\ %this_mod_folder_location%
 xcopy /R /Y /Q /S /E /I %git_mod_folder_location%expanded_decisions_propagate_bloodlines%git_mod_subfolder%expanded_decisions_propagate_bloodlines\ %this_mod_folder_location%
 xcopy /R /Y /Q /S /E /I %git_mod_folder_location%expanded_decisions_tributaries%git_mod_subfolder%expanded_decisions_tributaries\ %this_mod_folder_location%
